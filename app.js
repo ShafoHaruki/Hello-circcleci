085f115464cf15bf61e8c6a95a94f45458f8e67a
ShafoYoisho <muhammadsyafizuraimi@Gmail.com>
2019-10-24 16:26:03 +0800

    with yml

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+// var cors = require("cors");
+// app.use(cors());
 
 app.get("/", (req, res) => res.send("Hello world!"));
 module.exports = app;
